e8335376a0d88768d73155f08e33205e191f0f07
Joshua McMichael <josh@jmcmichael.com>
2020-03-29 21:10:32 -0500

    logo

--- src/index.js
+++ src/index.js
@@ -7,18 +7,3 @@ import 'bootstrap';
 jquery(() => {
   console.log('Hello jQuery + bootstrap 4!');
 });
-
-// require('normalize.css/normalize.css');
-// require('./styles/index.scss');
-
-// document.addEventListener("DOMContentLoaded", () => {
-
-//     const pluginsTriggerElement = document.getElementById('plugins-trigger');
-//     const pluginsElement = document.getElementById('plugins');
-
-//     const pluginsVisibleClass = "splash-overview-plugins__list--visible";
-
-//     pluginsTriggerElement.onclick = () => {
-//         pluginsElement.classList.toggle(pluginsVisibleClass);
-//     }
-// });
